e7abb762c6d5c46ee572d401f86f5d05c718f06a
kitaharasetusna <tlou2sucks@163.com>
2023-11-02 22:03:54 -0400

    add EXPSO3; add skew; init for propogate

--- src/IEKF.py
+++ src/IEKF.py
@@ -7,6 +7,24 @@ class InEKF:
         self.g_ = np.array([0, 0, -9.81])
         self.state_ = state
         self.noise_params_ = params
+    
+    
+    # phase 1: project ahead
+    # hat(X_{t+1}) = Phi@hat(X_{t})
+    # P^{-}_{k+1} = Phi_{k}@P_{k}
+    def propogate(self, m, dt):
+        w = m[:3]-self.state_.getGyroscopeBias()
+        a = m[3:]-self.state_.getAccelerometerBias() 
+
+        X = self.state_.getX() # (5+num_C,5+num_C) 
+        P = self.state_.getP_() # (15+num_C,15+num_C) TODO: update this comment
+
+        R = self.state_.getRotation() #(3,3)
+        v = self.state_.getVelocity() #(3,1)
+        p = self.state_.getPosition() #(3,1)
+
+        phi = w*dt
+        print(phi)
 
 
 #----------------------------------------------for testing
@@ -23,5 +41,12 @@ def testInEFKInit():
     print(inefk.noise_params_.getAccelerometerBiasCov())
     print(inefk.noise_params_.getContactCov())
 
+def testInEKFPropogate():
+    state = RobotState()
+    param = NoiseParam()
+    inefk = InEKF(state=state, params=param)
+    inefk.propogate(np.random.randn(6,1), dt=0.1)
+
 if __name__=="__main__":
-    testInEFKInit()
+    #testInEFKInit()
+    testInEKFPropogate()
--- src/matrixUtills.py
+++ src/matrixUtills.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+TOL_EXP = 1e-10
+
 # resize the shape of a matrix by returning a new matrix
 # all the padding will be 0
 def resizeNdarray(ori_matrix, desired_shape):
@@ -9,9 +11,23 @@ def resizeNdarray(ori_matrix, desired_shape):
     pad_matrix = np.pad(ori_matrix, ((0, pad_rows), (0, pad_cols)), mode='constant')
     return pad_matrix
 
+def skew(v):
+    M = np.zeros((3,3))
+    M[0, 1] = -v[2,0]; M[0, 2] = v[1,0]
+    M[1, 0] = v[2,0]; M[1, 2] = -v[0,0]
+    M[2, 0] = -v[1,0]; M[2, 1] = v[0,0]
+    return M
+    
 
-
-
+def EXPSO3(w):
+    A = skew(w)
+    theta = np.linalg.norm(A)
+    global TOL_EXP
+    if theta<TOL_EXP:
+       return np.eye(3) 
+    # Eular angle to rotation matrix
+    R = np.eye(3)+(np.sin(theta)/theta)*A + ((1-np.cos(theta))/(theta*theta))*A@A
+    return R 
 #----------------------------------------------for testing
 def test_resizeNdarray():
     randn_matrix = np.random.randn(6,7)
@@ -19,6 +35,17 @@ def test_resizeNdarray():
     resized_matrix = resizeNdarray(ori_matrix=randn_matrix, desired_shape=(8, 9))
     print(resized_matrix)
     print(randn_matrix.shape)
-    
+
+def test_skew():    
+    v_test = np.array([0.1,1,2]).reshape(-1,1)
+    v_x = skew(v_test)
+    print(v_x)
+
+def test_EXPSO3():
+    R = EXPSO3(np.random.randn(4,1))
+    print(R)
+
 if __name__=="__main__":
-    test_resizeNdarray()
+    # test_resizeNdarray()
+    # test_skew()
+    test_EXPSO3()
